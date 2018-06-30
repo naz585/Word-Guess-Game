d0d459ac1e14683ca2c1001efd703247d9feeebc
naz585 <naz585@aol.com>
2018-06-29 21:36:20 -0400

    added content

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,153 @@
 
+var words=["NATIVE AMERICANS", "STETSON HAT", "BANDANNA", "COWBOYS", "GUNFIGHTERS","REVOLVER","BOUNTY HUNTER", "SHERIFF", "WILD WEST", "BILLY THE KID", "BUFFALO BILL"];
+var Hints=["people", "clothing","clothing", "people", "people", "thing", "person", "person", "event", "person", "person"];
+var guessedLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]; 
 
-var names=["hello", "bye", "good"];
+var lives = 8;
 
-var rand = names[Math.floor(Math.random() * names.length)];
-var wordHolder = "?";
-console.log("guess the word");
+var currentWordindex;
 
-console.log(rand.length);
+var RandWord;
 
-var html2 = wordHolder.repeat(rand.length)
+var gamestart = false;
+
+var gameEnd = false;
+
+var wins = 0;
+
+var answer = [];
+
+var remainingLetters;
+
+function resetGame() {
+    lives = 8;
+    guessedLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    document.getElementById("letters").innerHTML = guessedLetters.join(" ");
+    RandWord = words[Math.floor(Math.random() * words.length)];
+    
+
+    answer = [];
+   
+    for (var i = 0; i < RandWord.length; i++) {
+        if (RandWord[i] == " ") {
+        answer.push("&nbsp;");
+        }    
+        else {
+        answer.push("_");
+        }
+    };
+    document.getElementById("word").innerHTML = answer.join(" ");
+    document.getElementById("hints").innerHTML = Hints[words.indexOf(RandWord)];
+    document.getElementById("lives").innerHTML ="lives: "+lives;
+    
+    remainingLetters = RandWord.length;
+    document.getElementById("winlose").innerHTML = "";
+   
+};
+    
+resetGame();
+document.getElementById("wins").innerHTML ="wins: " + wins;
+document.getElementById("letters").innerHTML = guessedLetters.join(" ");
 
 
-document.querySelector("#game2").innerHTML = html2;
+
+function checkGuess(letter) {
+    
+    for (var j = 0; j < RandWord.length; j++) {
+    if (RandWord[j] === letter) {
+        answer[j] = letter;
+        remainingLetters--;
+    
+        }
+    }
+    if (RandWord.match(letter) === null) {
+        lives--;
+
+        console.log(lives);
+        }
+        for (var i = 0; i < guessedLetters.length; i++){
+        if (guessedLetters[i] == letter) {
+       var Lnumber =  guessedLetters.indexOf(letter);
+       guessedLetters[Lnumber] = guessedLetters[Lnumber].fontcolor("red");
+        document.getElementById("letters").innerHTML = guessedLetters.join(" ");
+        
+    }   
+        }
+}
+    
+function checkWin() {
+    if (answer.indexOf("_") === -1){
+        document.getElementById("winlose").innerHTML = "you win press any key to start again";
+        wins++;
+        document.getElementById("wins").innerHTML ="wins: " + wins;
+        gameEnd = true;
+        }
+};
+    
+    
+  
+function checkLoss() {
+    if (lives <= 0) {
+        document.getElementById("winlose").innerHTML = "you lose press key to start again";
+        wins = 0;
+        document.getElementById("wins").innerHTML ="wins: " + wins;
+        gameEnd = true;
+        }
+};
+
+
 
 
 document.onkeyup = function(event) {
 
-   
-    var userGuess = event.key;
-  
+   userGuess = event.key
     
-for (var i=0; i<= rand.length; i++) {
-    if (userGuess === rand.charAt(i)) {
-        console.log(userGuess);
-        
-        
-        var userGeussN = rand.indexOf(userGuess);
-        console.log(userGeussN);
-        
-        var res = rand.match(userGuess);
-        
-        var replacer = html2.replace( html2.charAt(userGeussN),userGuess);
-       
-        html2 = replacer;
-        document.querySelector("#game2").innerHTML = html2;
-        
-        var html = 
-        "<p> You chose: " + res + "</p>";
-
-        
-         
-document.querySelector("#game").innerHTML = html;
-
-
+    if(gameEnd) {
+        resetGame();
+        gameEnd = false;
     }
+
+ 
+    else { if (event.keyCode >= 65 && event.keyCode <= 90) {
+    
+            checkGuess(userGuess.toUpperCase());
+            checkWin()
+            checkLoss()
+        }
+   
+    }
+
+    document.getElementById("word").innerHTML = answer.join(" ");
+    document.getElementById("lives").innerHTML ="lives: "+lives;
+};
     
 
-}
 
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
